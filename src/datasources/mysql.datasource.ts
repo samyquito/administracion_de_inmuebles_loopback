3a0f2b230cdc2ba6aa0e80bf512b5ac0507538e3
Juan Diego Ramirez <juan.1702011618@ucaldas.edu.co>
2022-04-22 20:48:12 -0500

    creando datasorce y conexión a base de datos

---
+++ src/datasources/mysql.datasource.ts
@@ -0,0 +1,31 @@
+import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
+import {juggler} from '@loopback/repository';
+
+const config = {
+  name: 'mysql',
+  connector: 'mysql',
+  url: '',
+  host: 'turismodb.mysql.database.azure.com',
+  port: 3306,
+  user: 'turismo_user',
+  password: 'Admi12345',
+  database: 'turismdb'
+};
+
+// Observe application's life cycle to disconnect the datasource when
+// application is stopped. This allows the application to be shut down
+// gracefully. The `stop()` method is inherited from `juggler.DataSource`.
+// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
+@lifeCycleObserver('datasource')
+export class MysqlDataSource extends juggler.DataSource
+  implements LifeCycleObserver {
+  static dataSourceName = 'mysql';
+  static readonly defaultConfig = config;
+
+  constructor(
+    @inject('datasources.config.mysql', {optional: true})
+    dsConfig: object = config,
+  ) {
+    super(dsConfig);
+  }
+}
