e0d2086f424290834ba829e78a6a9cc9bed5eed7
samyquito <samyquito02@gmail.com>
2022-05-26 21:16:46 -0500

    Eliminando conflcto de archovos, se deja solo general-data y se elimina general_date, se añade lo de general_date en generaÃl-data

--- src/config/general-data.ts
+++ src/config/general-data.ts
@@ -4,4 +4,7 @@ export namespace GeneralData {
   export const url_validator_token = 'http://localhost:2500/token-validator';
   export const URL_NOTIFICATIONS_SMS = 'http://localhost:5000/sms';
   export const hash_validator_notifications = 'Admin12345@2022Ucaldas';
+  export const filesFolder = '../../files/condominiums/';
+  export const filenameFieldCondominiums = 'file';
+  export const imageExtensions = [".SVG", ".PNG", ".JPG", ".JPEG"]
 }
